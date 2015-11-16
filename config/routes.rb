2663b0c68e0e8567c354edef9fbc03eb3d9dd4d5
UltanGriffin <u.griffin1@nuigalway.ie>
2015-11-16 18:20:13 +0000

    made vote page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'vote/index'
+
   resources :films
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
